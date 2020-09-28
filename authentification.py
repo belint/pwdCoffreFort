a9e10ecf3a8ae834850078e5c21184d3444cc838
anthonylafourcade <36070812+anthonylafourcade@users.noreply.github.com>
2020-09-28 20:27:00 +0200

    Authentification ok

--- authentification.py
+++ authentification.py
@@ -1,21 +1,38 @@
 import hashlib
-
+import mdpauth
 def main():
     # entrer les id et mdp
     print ("Connexion au coffre fort \n")
-    authentification()
-    # chiffrer le mdp
-    # ouvrir le fichier
+    (identifiant, authentifié) = authentification()
+    # print(authentifié)
+    while (authentifié == 0) :
+        print("Identifiant ou mot de passe incorrect. Réessayez \n")
+        (identifiant, authentifié) = authentification()
+    
+    print("Bienvenue " + identifiant + "\n")
+
 
 def authentification():
     identifiant = str(input("Identifiant : "))
     mdp = str(input("Mot de passe : "))
 
-    print('Identifiant = ' + identifiant + '\n')
-    print('mdp = ' + mdp + '\n')
-    mdphash = hashlib.sha256(mdp.encode('utf-8'))
+    # print('Identifiant = ' + identifiant + '\n')
+    # print('mdp = ' + mdp + '\n')
+    mdphash = (hashlib.sha256(mdp.encode('utf-8'))).hexdigest()
     # print('mdp hashé = ' + mdphash)
-    print(mdphash.hexdigest())
+    # print(mdphash)
     id = {identifiant : mdphash}
+    # print(id)
+    # print(mdpauth.auth)
+    # verifier que l'id est dans les les clés de mdpauth
+    if identifiant in mdpauth.auth.keys() :
+        if mdpauth.auth[identifiant] == mdphash :
+            authentifié = 1
+        else : 
+            authentifié = 0
+    else : 
+        authentifié = 0
+    
+    return (identifiant, authentifié)
 
 main()
